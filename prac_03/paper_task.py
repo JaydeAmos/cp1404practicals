166cc677daf2a7cacde5e5f0493fd568a4b66ceb
jayde <jayde.amos@my.jcu.edu.au>
2019-08-20 10:16:55 +1000

    Refactor password program to use functions

--- prac_03/paper_task.py
+++ prac_03/paper_task.py
@@ -2,6 +2,10 @@ MIN_LENGTH = 5
 
 
 def main():
+    get_password()
+
+
+def get_password():
     password = input("Enter password of at least 5 characters: ")
     while len(password) < MIN_LENGTH:
         password = input("Enter password of at least 5 characters: ")
