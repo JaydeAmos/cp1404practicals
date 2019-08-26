cb94664feeb8f606fc6bfca0560dd76b7cb7a192
jayde <jayde.amos@my.jcu.edu.au>
2019-08-27 09:40:32 +1000

    inserted starter code

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,17 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers)
+
+# numbers[0] - 3
+# numbers[-1] - 2
+# numbers[3] - 1
+# numbers[:-1] - 3, 1, 4, 1, 5, 9
+# numbers[3:4] - 1
+
+numbers[0] = "ten"
+print(numbers)
+
+numbers[6] = 1
+print(numbers)
+
+numbers[:-6]
+print(numbers)
