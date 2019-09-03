cf9b3d6785ad00b2bfcd6d14f5b9d9a1d6809525
jayde <jayde.amos@my.jcu.edu.au>
2019-09-03 10:24:22 +1000

    complete code for practicals

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,2 +1,13 @@
 word_count = {}
-user_input = input("Text: ")
+text = input("Text: ")
+words = text.split()
+for word in words:
+    frequency = word_count.get(word, 0)
+    word_count[word] = frequency + 1
+
+words = list(word_count.keys())
+words.sort()
+
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_length, word_count[word]))
